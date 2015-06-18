3fd3642d8bfb98f2a3c5c220f2a96a3bb0928e7c
squidgetx <squidgetx@gmail.com>
2015-06-18 07:04:58 -0400

    update

--- stuff.js
+++ stuff.js
@@ -10,3 +10,20 @@ var highlightEmail = function() {
   email.style.color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
 }
 
+
+window.onload = function() {
+  var c = document.getElementById("compliment");
+  var compliments = [
+    "Did you know you have enchanting eyes?",
+    "Did you know your hair looks awesome today?",
+    "The outfit you picked today looks great on you.",
+    "You have a very professional attitude.",
+    "The color of your eyes is really striking.",
+    "Did you know you give off a real aura of confidence?"
+      ];
+  var index = Math.floor(Math.random() * compliments.length);
+  var string = compliments[index];
+  c.innerHTML = string;
+  console.log(index);
+}
+
