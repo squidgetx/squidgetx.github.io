7ea3eda015d0696e18064e8bdcc3817dcf11d93b
Sylvan Zheng <sylvan.zheng@yale.edu>
2016-02-01 23:40:16 -0500

    fix colors

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 
 var randomColor = function() {
-  return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+  return '#'+(0x1000000+(Math.random())*0xeeeeee).toString(16).substr(1,6);
 }
 
 
